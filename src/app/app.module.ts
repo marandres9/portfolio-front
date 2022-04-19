a03f62ac3130bb86aaff5d3193d20cfcc2618e62
Mariano <perezmariano7401@gmail.com>
2022-04-18 22:30:31 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// router
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationExperienceComponent } from './components/education-experience/education-experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutComponent,
+        EducationExperienceComponent,
+        SkillsComponent,
+        ProjectsComponent,
+        MainComponent,
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            { path: 'home', component: MainComponent },
+            {path:'', redirectTo: '/home', pathMatch: 'full'},
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
