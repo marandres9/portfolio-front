c2125b632543a1751fc55a07c6f0896f7c19872c
Mariano <perezmariano7401@gmail.com>
2022-04-27 10:44:12 -0300

    using authService

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from 'src/app/authentication.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
     selector: 'app-about',
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthenticationService } from 'src/app/authentication.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
     selector: 'app-navbar',
