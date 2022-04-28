65a0aedd5aa98547cb4be95ded7feea824ca10e1
Mariano <perezmariano7401@gmail.com>
2022-04-28 18:35:34 -0300

    implemented experince-items in ed&ex-component

--- src/app/components/experience-item/experience-item.component.ts
+++ src/app/components/experience-item/experience-item.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-experience-item',
-  templateUrl: './experience-item.component.html',
-  styleUrls: ['./experience-item.component.css']
+    selector: 'app-experience-item',
+    templateUrl: './experience-item.component.html',
+    styleUrls: ['./experience-item.component.css']
 })
 export class ExperienceItemComponent implements OnInit {
 
-  constructor() { }
+    @Input() title: string = 'Senior graphic design specialist'
+    @Input() period: string = '2019 - Present'
+    @Input() company: string = 'a company'
+    @Input() location: string = 'Experion, New York, NY '
+    @Input() description: string
 
-  ngOnInit(): void {
-  }
+    items: string[]
+
+    constructor() {
+        this.description ='Lead in the design, development, and implementation of the graphic, layout, and production communication materials. Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design.  Oversee the efficient use of production project budgets ranging from $2,000 - $25,000'
+        this.items = this.description.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")
+    }
+
+    ngOnInit(): void {
+    }
 
 }
