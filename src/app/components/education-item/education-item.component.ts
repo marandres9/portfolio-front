179924f6305ca0f702d63ee67e7e33ca7f58b8ff
Mariano <perezmariano7401@gmail.com>
2022-04-28 11:37:39 -0300

    using separate education-item-component in ed&ex section

--- src/app/components/education-item/education-item.component.ts
+++ src/app/components/education-item/education-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-education-item',
-  templateUrl: './education-item.component.html',
-  styleUrls: ['./education-item.component.css']
+    selector: 'app-education-item',
+    templateUrl: './education-item.component.html',
+    styleUrls: ['./education-item.component.css']
 })
 export class EducationItemComponent implements OnInit {
 
-  constructor() { }
+    @Input() title: string = 'Master of Fine Arts & Graphic Design'
+    @Input() period: string = '2015 - 2016'
+    @Input() institution: string = 'Rochester Institute of Technology'
+    @Input() location: string = 'Rochester, NY'
+    @Input() description: string = 'Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatu qui ut dignissimos deleniti nerada porti sand markend'
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
