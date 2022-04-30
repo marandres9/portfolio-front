7b61c5dc7d4cca5008e30409fac43f41be10dd34
Mariano <perezmariano7401@gmail.com>
2022-04-30 13:01:36 -0300

    editing page receives portfolio from service

--- src/app/editing-page/editing-page.component.ts
+++ src/app/editing-page/editing-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Home } from '../model/Home';
+import { PortfolioDTO } from '../model/PortfolioDTO';
 import { HttpService } from '../service/http.service';
+import { PortfolioService } from '../service/portfolio.service';
 
 @Component({
     selector: 'app-editing-page',
@@ -10,29 +12,37 @@ import { HttpService } from '../service/http.service';
 })
 export class EditingPageComponent implements OnInit {
 
-    home = new Home();
+    portfolio = new PortfolioDTO();
 
     homeForm = new FormGroup({
-        title: new FormControl(''),
+        title: new FormControl(this.portfolio.home_title),
         description: new FormControl('')
     })
 
-    constructor(private http: HttpService) { }
+    constructor(
+        private http: HttpService,
+        private portfolioService: PortfolioService
+    ) { }
 
     ngOnInit(): void {
-        this.getHome()
+        if(this.portfolioService.isEmpty()) {
+            // make http req
+            console.log("portfolio is empty")
+            this.http.getPortfolio().subscribe(portfolio => {
+                this.portfolio = portfolio
+                console.log(this.portfolio)
+                this.updateForm()
+            })
+        } else {
+            // get from service
+            console.log("portfolio not empty")
+            this.portfolio = this.portfolioService.portfolio
+            this.updateForm()
+        }
+
     }
 
-
-    getHome() {
-        this.http.getHome().subscribe(home => {
-            this.home = home
-            this.updateFormContent()
-        })
-    }
-
-    updateFormContent() {
-        this.homeForm.get('title')?.setValue(this.home.title)
-        this.homeForm.get('description')?.setValue(this.home.description)
+    updateForm(): void {
+        this.homeForm.get('title')?.setValue(this.portfolio.home_title)
     }
 }
