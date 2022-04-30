d764dda999d504c566123f644d40ad169546a296
Mariano <perezmariano7401@gmail.com>
2022-04-30 13:01:28 -0300

    passes portfolioDto to service

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PortfolioDTO } from '../model/PortfolioDTO';
 import { HttpService } from '../service/http.service';
+import { PortfolioService } from '../service/portfolio.service';
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
     styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
 
     public portfoioDto = new PortfolioDTO();
 
-    constructor(private http: HttpService) { }
+    constructor(private http: HttpService, private portfolioService: PortfolioService) { }
 
     ngOnInit(): void {
         this.getPortfolio();
+    }
+
+    ngOnDestroy(): void {
+        this.portfolioService.portfolio = this.portfoioDto
     }
 
     public getPortfolio() {
         this.http.getPortfolio()
             .subscribe(portfolioDto => {
                 this.portfoioDto = portfolioDto
-                console.log(this.portfoioDto.projects)
             })
     }
 
