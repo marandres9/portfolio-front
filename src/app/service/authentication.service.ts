0cafa6ae4b9f0c9338d76d4c5181717dffa2a925
Mariano <perezmariano7401@gmail.com>
2022-05-23 10:14:31 -0300

    checks token expiration

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -15,45 +15,63 @@ export class AuthenticationService {
     // (using async pipe) and display content accordingly
     isLoggedIn = this._isLoggedIn.asObservable();
 
-    private readonly TOKEN_NAME = 'token'
+    private readonly TOKEN_NAME = 'token';
 
     constructor(private http: HttpService, private router: Router) {
-        // for page reloads, checks if a token is available and updates the login state
-        this._isLoggedIn.next(!!this.token)
-        // !!!INCOMPLETE - Should check token expiration
+        // for page reloads, checks if a token is available or expired and updates the login state
+        if (this.token) {
+            if (this.isTokenExpired(this.token)) {
+                this._isLoggedIn.next(false);
+            } else {
+                this._isLoggedIn.next(true);
+            }
+        } else {
+            this._isLoggedIn.next(false);
+        }
+    }
+
+    private isTokenExpired(token: string) {
+        const expiry = JSON.parse(atob(token.split('.')[1])).exp;
+        return Math.floor(new Date().getTime() / 1000) >= expiry;
     }
 
     login(loginReq: AuthenticationRequest) {
-        return this.http.login(loginReq).pipe(
-            // login() devuelve el observable de tipo AuthResponse
-            // el observable es encadenado con pipe para acceder a su valor con tap()
-            // Luego el observable sigue su rumbo normal
-            tap((authResponse) => {
-                console.log(authResponse)
-                if(typeof authResponse === 'object') {
-                    sessionStorage.setItem(this.TOKEN_NAME, authResponse['token']);
-                    // updates the login-state
-                    this._isLoggedIn.next(true)
-                }
-            },
+        return this.http
+            .login(loginReq)
+            .pipe(
+                // login() devuelve el observable de tipo AuthResponse
+                // el observable es encadenado con pipe para acceder a su valor con tap()
+                // Luego el observable sigue su rumbo normal
+                tap((authResponse) => {
+                    console.log(authResponse);
+                    if (typeof authResponse === 'object') {
+                        sessionStorage.setItem(
+                            this.TOKEN_NAME,
+                            authResponse['token']
+                        );
+                        // updates the login-state
+                        this._isLoggedIn.next(true);
+                    }
+                })
             )
-        ).pipe(catchError((err) => {
-            // si el usuario falla en la autenticacion el request y la funcion tap van a
-            // devolver errores que van a ser atrapados por esta funcion antes de devolver
-            // el error al componente de login. Si se atrapa un error se devuelve un arreglo
-            // vaio ([]) y no se actualiza el estado _isLoggedIn
-            return of([])
-        }));
+            .pipe(
+                catchError((err) => {
+                    // si el usuario falla en la autenticacion el request y la funcion tap van a
+                    // devolver errores que van a ser atrapados por esta funcion antes de devolver
+                    // el error al componente de login. Si se atrapa un error se devuelve un arreglo
+                    // vaio ([]) y no se actualiza el estado _isLoggedIn
+                    return of([]);
+                })
+            );
     }
 
     logout() {
         sessionStorage.removeItem(this.TOKEN_NAME);
         // remove token and update beahviour-subject
-        this._isLoggedIn.next(false)
+        this._isLoggedIn.next(false);
     }
 
     get token() {
-        return sessionStorage.getItem(this.TOKEN_NAME)
+        return sessionStorage.getItem(this.TOKEN_NAME);
     }
-
 }
