da1d69f74878123a5ec19e598b9731ea88e8d80a
Mariano <perezmariano7401@gmail.com>
2022-05-25 12:41:36 -0300

    about description now displays in paragraphs

--- src/app/content/about-content/about-content.component.ts
+++ src/app/content/about-content/about-content.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./about-content.component.css'],
 })
 export class AboutContentComponent implements OnInit {
-    @Input() description: string;
+    @Input() description: string[];
 
     constructor() {}
 
